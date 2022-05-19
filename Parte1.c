71b8324cc3e7f1e1239844a6f63fb198666af3b1
PedroRimes <pcoutinhorimes@gmail.com>
2022-05-18 23:10:16 -0300

    first commit

---
+++ Parte1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <threads.h>
+
+
+int main ()
+{
+    int n = 0, i;
+    printf("Digite a quantidade desejada de threads = ");
+    scanf("%d", &n);
+    thrd_t names[n];
+
+
+    for(i = 0; i < n; i++)
+    {
+        
+    }
+
+}
