c195602a8bfbc849b8a49a577d9126f52c3c2993
PedroRimes <pcoutinhorimes@gmail.com>
2022-05-19 20:35:57 -0300

    Created commentary in portuguese

--- Parte1.2.c
+++ Parte1.2.c
@@ -1,10 +1,15 @@
+// Aqui o código é igual, a unica difrenca é a linha comentada a baixo.
+
+//O gettid pega o ID definido pelo kernel, esse ID é proximo do ID do processo, o mesmo ID que pode ser visto usando htop no linux
+//Ja o thrd_current cria um ID unico para a thread, porem, esse ID pode ser re-utilizado quando a thread é terminada.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
 
 void *imprime(void *i){
     printf("Eu sou a thread %ld e meu ID é %lu.\n",++i,thrd_current());
-    printf("Usando getid, a thread %ld terá o ID %lu.\n",i,gettid());
+    printf("Usando getid, a thread %ld terá o ID %lu.\n",i,gettid()); // Uso do gettid
 }
 
 
--- Parte1.c
+++ Parte1.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <threads.h>
+#include <threads.h> //biblioteca requisitada
 
-void *imprime(void *i){
-    printf("Eu sou a thread %ld e meu ID é %lu.\n",++i,thrd_current());
+
+void *imprime(void *i)//função para imprimir as threads e o seu ID usando thrd_current como requisitado
+{
+    printf("Eu sou a thread %ld e meu ID é %lu.\n",++i,thrd_current());//thrd_current é usado para pegar o ID que o sistema operacional da para a thread
 }
 
 
-int main (void)
+int main (void)//main é criada
 {
-    int n = 0, i;
+    int n = 0, i;// criado a variavel que vai receber os nomes e agir como contador
     printf("Digite a quantidade desejada de threads = ");
     scanf("%d", &n);
-    thrd_t threads[n];
-
-
+    thrd_t threads[n];// é definido as threads
     for(i = 0; i < n; i++)
     {
-        thrd_create(&threads[i], imprime,(void *)i);
+        thrd_create(&threads[i], imprime,(void *)i);//é feito a criação das threads juntamente da chamada da função com o print para o usuario
     }
-
-    thrd_exit(0);
-
+    thrd_exit(0); //codigo criado para fechar a thread
+    // poderia usar join, mas acredito estar errado, join apenas bloqueia, exit termina.
 }
+
+// Um ponto interessante em relação ao código, é que as threads se sobrepoem durante o print.
+
+//https://en.cppreference.com/w/c/thread
