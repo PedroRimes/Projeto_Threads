4df9f4ef51727fe539aa1a4c5b293373935e96be
PedroRimes <pcoutinhorimes@gmail.com>
2022-05-20 16:50:17 -0300

    char changed

--- Parte1.2.c
+++ Parte1.2.c
@@ -2,20 +2,21 @@
 
 //O gettid pega o ID definido pelo kernel, esse ID é proximo do ID do processo, o mesmo ID que pode ser visto usando htop no linux
 //Ja o thrd_current cria um ID unico para a thread, porem, esse ID pode ser re-utilizado quando a thread é terminada.
-
+#include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
 
-void *imprime(void *i){
-    printf("Eu sou a thread %ld e meu ID é %lu.\n",++i,thrd_current());
-    printf("Usando getid, a thread %ld terá o ID %lu.\n",i,gettid()); // Uso do gettid
+void imprime(void *name){
+    printf("Eu sou a %s e meu ID é %lu.\n",name,thrd_current());
+    printf("Usando getid, a %s terá o ID %lu.\n",name,gettid()); // Uso do gettid
 }
 
 
 int main (void)
 {
     int n = 0, i;
+    char name[10];
     printf("Digite a quantidade desejada de threads = ");
     scanf("%d", &n);
     thrd_t threads[n];
@@ -23,7 +24,8 @@ int main (void)
 
     for(i = 0; i < n; i++)
     {
-        thrd_create(&threads[i], imprime,(void *)i);
+        sprintf(name, "thread_%d", i);
+        thrd_create(&threads[i], imprime,(void *)name);
     }
 
     thrd_exit(0);
--- Parte1.c
+++ Parte1.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <threads.h> //biblioteca requisitada
 
 
-void *imprime(void *i)//função para imprimir as threads e o seu ID usando thrd_current como requisitado
+void imprime(void *name)//função para imprimir as threads e o seu ID usando thrd_current como requisitado
 {
-    printf("Eu sou a thread %ld e meu ID é %lu.\n",++i,thrd_current());//thrd_current é usado para pegar o ID que o sistema operacional da para a thread
+    printf("Eu sou a %s e meu ID é %lu.\n",name,thrd_current());//thrd_current é usado para pegar o ID que o sistema operacional da para a thread
 }
 
 
 int main (void)//main é criada
 {
     int n = 0, i;// criado a variavel que vai receber os nomes e agir como contador
+    char name[10];
     printf("Digite a quantidade desejada de threads = ");
     scanf("%d", &n);
     thrd_t threads[n];// é definido as threads
     for(i = 0; i < n; i++)
     {
-        thrd_create(&threads[i], imprime,(void *)i);//é feito a criação das threads juntamente da chamada da função com o print para o usuario
+        sprintf(name, "thread_%d", i);
+        thrd_create(&threads[i], imprime,(void *)name);//é feito a criação das threads juntamente da chamada da função com o print para o usuario
     }
     thrd_exit(0); //codigo criado para fechar a thread
     // poderia usar join, mas acredito estar errado, join apenas bloqueia, exit termina.
